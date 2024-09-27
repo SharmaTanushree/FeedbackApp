491585580b9c2053b8eddbf9991705bc51657a53
SharmaTanushree <tanushreesharma1998@gmail.com>
2024-09-28 01:37:59 +0530

    adding more styles

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import { Link} from 'react-router-dom'
+import Card from '../components/Card'
 
 function About() {
   
   return (
+    <Card>
     <div className='about'>
-        <h2>Its a feedback application</h2>
+        <h1>Its a feedback application</h1>
+        <p>This is a simple app to give feedback</p>
+        <p>
         <Link to='/'>Home</Link>
+        </p>
     </div>
+    </Card>
   )
 }
 
